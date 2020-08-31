14536b6e5c02d0c08394220f45ea921f8d0f12a0
Matías Irala <matias.irala.aveiro@gmail.com>
2020-08-31 08:33:30 -0400

    Cambia a OpenMP

--- src/tsp-acs.c
+++ src/tsp-acs.c
@@ -1,135 +1,176 @@
 #include "tsp-acs.h"
 
-int M;
+int M;	//Número de hormigas
 
-double** distanceMatrix;
-double** tau;
-int** cl;
-InstanceData ins;
-Ant ant[1000];
+double** distanceMatrix;	//Almacena la distancia entre todos los nodos
+double** tau;				//tau[i][j] representa el nivel de feromonas en el vértice(i,j)
+int** cl;					//Matriz con todos los nodos (filas) y sus CLSIZE nodos más cercanos (columnas) ordenados de forma descendiente
+InstanceData ins;			//Instacia TSP
+Ant* ant;					//Matriz con el tour y los nodos visitados de cada hormiga
 
 int runParallel;
 
 int main(int argc, char *argv[]) {
-	float bestKnownCost = FLT_MAX;
-	int* bestKnownTour;
+	// Timer start
+	struct timespec ts_start;
+	clock_gettime(CLOCK_MONOTONIC, &ts_start);
+
+	float bestKnownCost = FLT_MAX;	//Almacena el mejor costo del tour entre las hormigas
+	int* bestKnownTour;				//Almacena el mejor tour entre las hormigas
 
 	FILE *file;
 	int i, j, k;
-	file = fopen(argv[1], "r");
-	M = atoi(argv[2]);
-	runParallel = atoi(argv[3]);
-	fscanf(file, "NAME : %[^\n]s", ins.name);
-	fscanf(file, "\nCOMMENT : %[^\n]s", ins.comment);
-	fscanf(file, "\nTYPE : %[^\n]s", ins.type);
-	fscanf(file, "\nDIMENSION : %d", &ins.dimension);
-	fscanf(file, "\nEDGE_WEIGHT_TYPE : %[^\n]s", ins.wtype);
-	fscanf(file, "\nNODE_COORD_SECTION");
-	if (strcmp(ins.wtype,"EUC_2D")) {
-		return 1;
+	
+	//argv[1] indica el directorio al archivo que guarda los datos de una instancia de TSP
+	//El archivo posee una extensión .tsp, cuyo formato es TSPLIB
+	file = fopen(argv[1], "r"); //Se abre el archivo con la instancia de TSP							
+	
+	M = atoi(argv[2]);			//argv[2] indica el número de hormigas a enrutar
+	
+	runParallel = atoi(argv[3]);//argv[3] es un booleano que indica la paralización con hilos, 0 o 1
+	
+	//Se extraen los datos del archivo con la instancia TSP
+	fscanf(file, "NAME : %[^\n]s", ins.name);				//ins.name almacena el nombre de la instancia TSP
+	fscanf(file, "\nCOMMENT : %[^\n]s", ins.comment);		//ins.comment almacena un comentario adicional
+	fscanf(file, "\nTYPE : %[^\n]s", ins.type);				//ins.type almacena el tipo de la instancia, en este caso "TSP"
+	fscanf(file, "\nDIMENSION : %d", &ins.dimension);		//ins.dimension almacena el número de nodos
+	fscanf(file, "\nEDGE_WEIGHT_TYPE : %[^\n]s", ins.wtype);//ins.wtype almacena cómo los pesos de borde están dadas, en este caso "EUC_2D", distancia euclidiana en 2-D
+	fscanf(file, "\nNODE_COORD_SECTION");					//especifica las coordenadas asociada a cada nodo
+	
+	if (strcmp(ins.wtype,"EUC_2D")) {	//Comprueba que el tipo de los pesos de borde sea "EUC_2D"
+		return 1;						//Si no es EUC_2D se cancela la ejecución retornando 1
 	}
-	if (runParallel) {
+
+	if (runParallel) {					//Si se ejecuta en paralelo 
 		printf("Running parallel version\n");
 	}
-	else {
+	else {								//Si se ejecuta en secuencial 
 		printf("Running standard version\n");
 	}
-	int** coord;
-	coord = (int**) malloc(ins.dimension * sizeof(int*));
-	bestKnownTour = (int*) malloc(ins.dimension * sizeof(int));
+
+	int** coord;	//coord almacenará las coordenadas de los nodos
+	coord = (int**) malloc(ins.dimension * sizeof(int*));	//Reserva memoria para las filas, una fila por nodo
+	bestKnownTour = (int*) malloc(ins.dimension * sizeof(int));	//Almacena el mejor tour entre las hormigas
+	
+	//Por cada una de las filas reserva memoria para dos columnas, coordenadas x e y del nodo
 	for(i = 0; i <ins.dimension; i++) {
 		coord[i] = (int*) malloc(2 * sizeof(int));
 	}
+
+	//Se lee las coordenadas desde el archivo y se almacena en la matriz coord 
 	for (i = 0; i < ins.dimension; i++) {
 		fscanf(file, "\n %*[^ ] %d %d", &coord[i][0], &coord[i][1]);
 	}
-	fclose(file);
+	fclose(file);	//Cierra el archivo
 	
-	distanceMatrix = (double**) malloc(ins.dimension * sizeof(double*));
+	//distanceMatrix es una matriz con la distancia entre todos los nodos
+	distanceMatrix = (double**) malloc(ins.dimension * sizeof(double*));		//Reserva memoria para la cantidad de nodos en fila
 	for (i = 0; i < ins.dimension; i++) {
-		distanceMatrix[i] = (double*) malloc(ins.dimension * sizeof(double));
+		distanceMatrix[i] = (double*) malloc(ins.dimension * sizeof(double));	//Por cada fila reserva memoria para la cantidad de nodos en columna
 	}
 	
+	//Calcula la distancia entre todos los nodos y lo almacena en distanceMatrix
 	for (i = 0; i < ins.dimension; i++){
 		for ( j = i + 1; j < ins.dimension ; j++) {
 			distanceMatrix[i][j] = dist(coord[i][0], coord[i][1], coord[j][0], coord[j][1]);
 			distanceMatrix[j][i] = distanceMatrix[i][j];
 		}
 	}
-	free(coord);
+	free(coord);	//Se libera la matriz con las coordenadas de los nodos
 
-	tau = (double**) malloc(ins.dimension*sizeof(double*));
+	//tau es una matriz con el nivel de feromonas en los vértices entre todos los nodos
+	tau = (double**) malloc(ins.dimension*sizeof(double*));			//Reserva memoria para la cantidad de nodos en fila
 	for (i = 0; i < ins.dimension; i++) {
-		tau[i] = (double*) malloc(ins.dimension * sizeof(double));
+		tau[i] = (double*) malloc(ins.dimension * sizeof(double));	//Por cada fila reserva memoria para la cantidad de nodos en columna
 	}
+
+	//Inicializa todos los vértices con el nivel de feromona inicial TAUO
 	for (i = 0; i < ins.dimension; i++) {
 		for( j = 0; j < ins.dimension; j++) {
 			tau[i][j] = TAU0;
 		}
 	}
 	
-	srand(time(NULL));
-	for (i = 0; i < M; i++) {
-		ant[i].tour = (int*) malloc(ins.dimension * sizeof(int));
-		ant[i].tour[0] = rand() % ins.dimension;
-		ant[i].visited = (int*) malloc(ins.dimension * sizeof(int));
-		for (j = 1; j < ins.dimension; j++) {
+	srand(time(NULL));	//Establece la hora como la semilla del rand
+	
+	ant = (Ant*) malloc(M * sizeof(Ant));	//Reserva las filas para almacenar el tour y nodos visitados de cada hormiga
+	
+	//Inicializa las hormigas 
+	for (i = 0; i < M; i++) {	//Por cada hormiga
+		ant[i].tour = (int*) malloc(ins.dimension * sizeof(int));	//Reserva memoria para el tour de la hormiga
+		ant[i].tour[0] = rand() % ins.dimension;					//random para establecer la posición inicial de la hormiga
+		ant[i].visited = (int*) malloc(ins.dimension * sizeof(int));//Reserva memoria para los nodos visitados de la hormiga
+		for (j = 1; j < ins.dimension; j++) {						//Indica que al comienzo la hormiga no visitó ningún nodo
 			ant[i].visited[j] = 0;
 		}
-		ant[i].visited[ant[i].tour[0]] = 1;
+		ant[i].visited[ant[i].tour[0]] = 1;							//Indica como visitado la posición inicial de la hormiga
 	}
 	
-	cl = (int**) malloc(ins.dimension * sizeof(int*));
-	int* visited = (int*) malloc(ins.dimension * sizeof(int));
-	double nearestDistance;
-	int nearestNeighbor;
+	cl = (int**) malloc(ins.dimension * sizeof(int*));				//Reserva filas para la cantidad de nodos
 	for (i = 0; i < ins.dimension; i++) {
-		cl[i] = (int*) malloc(CLSIZE * sizeof(int));
+		cl[i] = (int*) malloc(CLSIZE * sizeof(int));				//Por cada fila de cl, reserva CLSIZE columnas
 	}
-	for (i = 0; i < ins.dimension; i++) {
-		for(j = 0; j < ins.dimension; j++) {
+	int* visited = (int*) malloc(ins.dimension * sizeof(int));		//Vector auxiliar de visitados
+	double nearestDistance;		//Menor distancia a un nodo
+	int nearestNeighbor;		//Nodo con menor distancia
+
+	/*
+	* Cada fila de la matriz cl representa un nodo, cada fila posee CLSIZE columnas.
+	* Las columnas representan los CLSIZE nodos vecinos más cercanos ordenados de forma ascendente (de izquierda a dercha)
+	* En el siguiente for se carga los valores de la matriz cl
+	*/
+	for (i = 0; i < ins.dimension; i++) {	
+		for(j = 0; j < ins.dimension; j++) {	
 			visited[j] = 0;
 		}
-		for(j = 0; j < CLSIZE; j++) {
-			nearestDistance = DBL_MAX;
-			nearestNeighbor = 0;
-			for(k = 0; k < ins.dimension; k++) {
+		for(j = 0; j < CLSIZE; j++) {	
+			nearestDistance = DBL_MAX;	
+			nearestNeighbor = 0;	
+			for(k = 0; k < ins.dimension; k++) {	
+				//Si el nodo no fue visitado y la distancia a ese nodo es aún menor y no es el nodo mismo
 				if(!visited[k] && distanceMatrix[i][k] < nearestDistance && i != k) {
-					nearestDistance = distanceMatrix[i][k];
-					nearestNeighbor = k;
+					nearestDistance = distanceMatrix[i][k];	//Actualiza la menor distancia
+					nearestNeighbor = k;					//Indica cuál es el nodo con menor distancia
 				}
 			}
-			visited[nearestNeighbor] = 1;
-			cl[i][j] = nearestNeighbor;
+			visited[nearestNeighbor] = 1;	
+			cl[i][j] = nearestNeighbor;	
 		}
 	}
 	
-	pthread_t *thread;
-	thread = (pthread_t*) malloc(M * sizeof(pthread_t));
-	
-	for (i = 0; i < ITERATIONS; i++) {
-		if (runParallel) {
-			for(j = 0; j < M; j++) {
-				int st = pthread_create(&thread[j], NULL, routeAnt, (void *) j);
-				if (st) {
-					printf("pthread_create() error %d\n",st);
-					exit(-1);
+	for (i = 0; i < ITERATIONS; i++) {	//Realiza las iteraciones
+		if (runParallel) {	//Modo Paralelo
+			//HotSpot
+			#pragma omp parallel
+			{
+				#pragma omp single nowait
+				{
+					if(!i) {	//Para la primera iteración y el primer hilo imprime la cantidad de hilos
+						int num_threads = omp_get_num_threads();
+						printf("Number of threads: %d\n", num_threads);
+					}
+				}
+				#pragma omp for
+				for (j = 0; j < M; j++) {	//Divide la cantidad total de hormigas entre la cantidad de núcleos 
+					for (k = 1; k < ins.dimension; k++) {					//Por cada movimiento a un nodo
+						if (hasCandidatesLeft(j,ant[j].tour[k-1])) {		//Si hay un nodo entre los CLSIZE más cercanos no visitado
+							moveAntTo(j, k, argMax(j, ant[j].tour[k-1]));	//Calcula y mueve la hormiga según la fórmula de probabilidad
+						}
+						else {												//Si ha visitado todos los CLSIZE nodos más cercanos
+							moveAntTo(j, k, NN(j,k-1));						//Mueve la hormiga simplemente al nodo más cercano restante
+						}
+					}
 				}
 			}
-		
-			for (j = 0; j < M; j++) {
-				pthread_join(thread[j],NULL);
-			}
-			
 		}
-		else {
-			for (j = 0; j < M; j++) {
-				for (k = 1; k < ins.dimension; k++) {
-					if (hasCandidatesLeft(j,ant[j].tour[k-1])) {
-						moveAntTo(j, k, argMax(j, ant[j].tour[k-1]));
+		else {	//Modo secuencial
+			for (j = 0; j < M; j++) {									//Por cada hormiga
+				for (k = 1; k < ins.dimension; k++) {					//Por cada movimiento a un nodo
+					if (hasCandidatesLeft(j,ant[j].tour[k-1])) {		//Si hay un nodo entre los CLSIZE más cercanos no visitado
+						moveAntTo(j, k, argMax(j, ant[j].tour[k-1]));	//Calcula y mueve la hormiga según la fórmula de probabilidad
 					}
-					else {
-						moveAntTo(j, k, NN(j,k-1));
+					else {												//Si ha visitado todos los CLSIZE nodos más cercanos
+						moveAntTo(j, k, NN(j,k-1));						//Mueve la hormiga simplemente al nodo más cercano restante
 					}
 				}
 			}
@@ -137,15 +178,18 @@ int main(int argc, char *argv[]) {
 		
 		int bestAnt = 0;
 		double lowerCost = DBL_MAX;
+		//Encuentra el tour con menor costo
 		for (j = 0; j < M; j++) {
 			if (tourCost(ant[j].tour) < lowerCost) {
 				bestAnt = j;
 				lowerCost = tourCost(ant[j].tour);
 			}
 		}
-
+		
+		//Actualiza los niveles de feromonas
 		updatePheromoneLevel(ant[bestAnt].tour);
 		
+		//Si en esta iteracion se encuentra un tour con menor costo se guarda
 		if (lowerCost < bestKnownCost) {
 			bestKnownCost = lowerCost;
 			for (k = 0; k < ins.dimension; k++) {
@@ -153,6 +197,7 @@ int main(int argc, char *argv[]) {
 			}
 		}
 
+		//Reinicia las hormigas
 		for (j = 0; j < M; j++) {
 			for (k = 0; k < ins.dimension; k++) {
 				ant[j].visited[k] = 0;
@@ -169,21 +214,26 @@ int main(int argc, char *argv[]) {
 	for (j = 0; j < ins.dimension; j++) {
 		printf("%d ", bestKnownTour[j]);
 	}
-	printf("; Length = %f\n", bestKnownCost);
+	printf("%d ", bestKnownTour[0]);
+	printf("\nLength = %f\n", bestKnownCost);
 
+	free(ant);
+
+	// timer stop
+	struct timespec ts_stop;
+	clock_gettime(CLOCK_MONOTONIC, &ts_stop);
+	double start = (double)ts_start.tv_sec + (double)ts_start.tv_nsec/1000000000.0;
+	double stop = (double)ts_stop.tv_sec + (double)ts_stop.tv_nsec/1000000000.0;
+	double elapsed = (stop - start);
+
+	// display time
+	printf ("Time = %f\n", elapsed);
 	return 0;
 }
 
-void *routeAnt(void *id) {
-	int j, i = (int)id;
-	for(j=1;j<ins.dimension;j++){
-		if(hasCandidatesLeft(i,ant[i].tour[j-1]))
-			moveAntTo(i,j,argMax(i,ant[i].tour[j-1]));
-		else
-			moveAntTo(i,j,NN(i,j-1));
-	}
-}
-
+/*
+* Actualiza los niveles de feromonas
+*/
 void updatePheromoneLevel(int* tour) {
 	int i;
 	for (i = 1; i < ins.dimension; i++) {
@@ -192,6 +242,9 @@ void updatePheromoneLevel(int* tour) {
 	}
 }
 
+/*
+* Dada una hormiga y su nodo actual, calcula y retorna el nodo no visitado más cercano
+*/
 int NN(int antIndex, int origin) {
 	double nearestDistance = DBL_MAX;
 	int nearestNeighbor = 0;
@@ -205,6 +258,9 @@ int NN(int antIndex, int origin) {
 	return nearestNeighbor;
 }
 
+/*
+* Dada una hormiga, el slot actual en su tour y el siguiente nodo a visitar, mueve la hormiga al destino y actualiza los niveles de feromonas
+*/
 void moveAntTo(int antIndex, int slot, int city){
 	ant[antIndex].tour[slot] = city;
 	ant[antIndex].visited[city] = 1;
@@ -212,6 +268,9 @@ void moveAntTo(int antIndex, int slot, int city){
 	tau[ant[antIndex].tour[slot-1]][ant[antIndex].tour[slot]] = (1-RHO) * tau[ant[antIndex].tour[slot-1]][ant[antIndex].tour[slot]] + RHO * TAU0;
 }
 
+/*
+* Dada una hormiga y su nodo actual, calcula y retorna el nodo a donde se moverá la hormiga según la fórmula de probabilidad
+*/
 int argMax(int antIndex, int currentCity){
 	int i, bestCity;
 	float maxu, Tau, Nu;
@@ -230,13 +289,17 @@ int argMax(int antIndex, int currentCity){
 	return bestCity;
 }
 
+/*
+* Dada una hormiga y su nodo actual, verifica si hay algún nodo cercano que todavía no ha visitado
+* En caso de encontrar un nodo cercano no visitado retorna 1, en caso contrario 0
+*/
 int hasCandidatesLeft(int antIndex, int currentCity) {
 	int i;
 	int clvisited[CLSIZE];
-	for (i = 0; i < CLSIZE; i++) {
+	for (i = 0; i < CLSIZE; i++) {	//Verifica si la hormiga ya visitó los nodos más cercanos de su nodo actual
 		clvisited[i] = ant[antIndex].visited[cl[currentCity][i]];
 	}
-	for (i = 0; i < CLSIZE; i++) {
+	for (i = 0; i < CLSIZE; i++) {	//Si hay un nodo cercano que no ha visitado entonces retorna 1
 		if (!clvisited[i]) {
 			return 1;
 		}
@@ -244,6 +307,9 @@ int hasCandidatesLeft(int antIndex, int currentCity) {
 	return 0;
 }
 
+/*
+* Calcula la distancia total entre dos coordenadas 
+*/
 float dist(int x1, int y1, int x2, int y2) {
         float vdist, hdist;
         if (x1 > x2) vdist = (float)x1 - (float)x2;
@@ -253,6 +319,9 @@ float dist(int x1, int y1, int x2, int y2) {
         return sqrtf((float)(pow(hdist,2) + pow(vdist,2)));
 }
 
+/*
+* Calcula es costo total asociado al tour de una hormiga
+*/
 double tourCost(int* tour) {
 	int i;
 	double cost = (double)0;
--- src/tsp-acs.h
+++ src/tsp-acs.h
@@ -5,18 +5,26 @@
 #include <time.h>
 #include <float.h>
 #include <unistd.h>
-#include <pthread.h>
-#include <semaphore.h>
+#include <omp.h>
 
+//Cantidad total de iteraciones
 #define ITERATIONS	10000
+//CLSIZE es la cantidad de nodos cercanos que la hormiga tendrá en cuenta
 #define CLSIZE		15
-#define Q0			0.9
+//Alpha representa el parámetro de influencia de feromona
 #define ALPHA		1
+//Beta representa el parámetro de influencia de distancia
 #define BETA		2
+//TAUO representa los valores de feromonas iniciales en los vértices
 #define TAU0		0.00001
+//RH0 es la tasa de evaporación de feromonas
 #define RHO			0.5
+//Q es una constante para actualizar los niveles de feromonas
 #define Q			100
 
+/*
+* Estructura para almacenar los datos de la instacia TSP
+*/
 typedef struct {
 	char name[100];
 	char comment[100];
@@ -25,6 +33,9 @@ typedef struct {
 	char wtype[100];
 } InstanceData;
 
+/*
+* Estructura para guardar el tour y los nodos visitados por una hormiga
+*/
 typedef struct {
 	int* tour;
 	int* visited;
@@ -37,4 +48,3 @@ int argMax(int, int);
 int NN(int, int);
 void moveAntTo(int, int, int);
 void updatePheromoneLevel(int*);
-void *routeAnt(void *);
